f56f1a393623244384f85c10de00b7b27d6c2a46
chylinski.t <chylinski.t@outlook.com>
2019-12-27 23:11:21 -0500

    feat(word-guess): added word.js functionality

---
+++ word.js
@@ -0,0 +1,27 @@
+var letter = require("letter.js");
+
+
+function word(answer){
+
+    this.objArrary = [];
+    for(var i = 0; i <answer.length; i++){
+        var cpuLetter = new letter(answer[i])
+        this.objArrary.push(cpuLetter)
+
+    }
+    this.log = function(){
+        var answerLog = "";
+        for(var i = 0; i < this.objArrary.length; i++){
+            answerLog += this.objArrary[i] + " ";
+        }
+        console.log(answerLog + "\n========================\n")
+    }
+    this.userGuess = function(input){
+        for(var i; i < this.objArrary.length; i++){
+            this.objArrary[i].guess(input);
+        }
+    }
+
+}
+
+module.export = word;
