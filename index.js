b2ab2da975fbafa2c638bd2f3971caac334b4267
chylinski.t <chylinski.t@outlook.com>
2019-12-29 19:37:09 -0500

    feat(word-guess): added guess words

---
+++ index.js
@@ -0,0 +1,29 @@
+var word = require("word.js");
+var letter = require("letter.js");
+
+
+var letterArrary = "";
+
+var cars = [
+    "ferrari",
+    "toyota",
+    "chevrolet",
+    "honda",
+    "lexus",
+    "acura",
+    "hyundai",
+    "volvo",
+    "audi",
+    "volkswagon",
+    "nissan",
+    "mercedas",
+    "subaru",
+    "land rover",
+    "jeep",
+    "lamborghini",
+    "bentley",
+    "chrystler",
+    "fiat",
+    "suzuki",
+    "bmw"
+];
